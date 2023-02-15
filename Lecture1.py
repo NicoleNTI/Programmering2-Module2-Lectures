cd7927bdfd668f4161725cfea8bfdf9f3196303f
Nicole <nicole.polis@ga.ntig.se>
2023-02-15 10:28:32 +0100

    Lecture1

---
+++ Lecture1.py
@@ -0,0 +1,56 @@
+######### Läsa in från fil: the basics ############
+
+# Att öppna fil
+
+filePath = "Programmering2-Module2-Lectures/PythonPoem.txt"
+
+infil = open(filePath, "r")
+
+# läs in en rad från fil
+
+s = infil.readline()
+
+s2 = infil.readline()
+
+
+# läsa in specifikt antal tecken
+
+s3 = infil.read(15)
+
+
+# läsa in hela filen till en lista
+
+list = infil.readlines()
+print(list)
+
+# läs in alla rader i filen till en lista av rader
+
+
+# Stäng filen
+infil.close()
+
+# Hantera slutet av en fil
+
+infil = open(filePath, "r")
+nextLine = infil.readline()
+while nextLine != "":
+    nextLine = infil.readline()
+    print(nextLine)
+
+# Man kan också läsa från fil med for-loop, det föredrar vi, då slipper vi stänga filen efteråt!
+
+with open(filePath, "r", encoding="UTF-8") as file:
+    for line in file:
+        print(line)
+
+######### skriva ut in på fil ############
+
+utfil = open("ny.txt", "w")
+
+utfil.write("Hej :D")
+utfil.write("Hej då! :(")
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Programmering2-Module2-Lectures
+## Här lägger jag ut koden jag visat under genomgångarna
+
+Planering
+Lektion 1 - Onsdag 15 Februari: Intro Filer
+Lektion 2 - Torsdag 16 Februari:
+Lektion 3 - Torsdag 16 Februari:
+Lektion 4 - Onsdag 22 Februari:
+Lektion 5 - Torsdag 23 Februari:
+Lektion 6 - Torsdag 23 Februari: 
